46626fcbc736104edd6f82bbb72b8a38f8ba3cc0
Andrei Pestenariu <pclandrei@gmail.com>
2019-12-16 01:11:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num == 1 || num == 0 || num < 0
+    return false  
+  elsif num <= 1 
+    return true 
+  end
+  range = (2..num -1).to_a 
+  
+  range.any? { |x|
+    if num % x == 0
+      return false
+    end
+  }
+    true
+ 
+end

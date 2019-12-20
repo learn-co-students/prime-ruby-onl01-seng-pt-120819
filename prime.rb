ee5360061d0a9e5a48b0f23765fa432b439a2efc
kowalix007 <kowalix007@gmail.com>
2019-12-20 00:11:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+require "pry"
+def prime? (number)
+  return false if number <= 1
+  numbers = (2...number).to_a 
+  numbers.all? do |num|
+  # binding.pry
+   number%num != 0 
+  end
+  #if numbers.ea is > 1 
+  #elsif  number/number == Integer
+   # return true
+  #else 
+   #return false 
+  end
+
+  # Add  code here!

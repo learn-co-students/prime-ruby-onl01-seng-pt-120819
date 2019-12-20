2358b235933bc3fadf390f21e9e8ad543cb0feef
Asif Kamal <asif_kamal@hotmail.com>
2019-12-20 21:55:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+
+array = (1..num).to_a
+
+ array.each do |d|
+   if array.select { |d| num % d == 0 } == [1, num]
+      return true 
+    else
+      return false
+   end
+end
+if num < 2 
+  return false
+end
+end

a443867e7c7aa7310ec67cbe46cebedf8649611a
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-21 03:49:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?
+  num== 2
+  if num/2 <=2
+    return "false"
+    num==[1..100]
+    num== num+1
+    return Prime_number
+
+  elsif num==Prime_number
+    puts"true"
+  else puts "false"
+  end

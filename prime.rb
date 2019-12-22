1ea368d7e9920428f1c5e1ea3b9f2d2595a822d7
Damely Tineo <damely.tineo07@gmail.com>
2019-12-22 18:11:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+
+def prime?(num)
+  array = (1..num).to_a  
+  if num < 2 
+    return false
+  end
+  array.each do |x|
+    if array.select {|x| num % x == 0} == [1, num]
+      return true
+    else 
+      return false
+    end
+  end
+end
+  
+
+  

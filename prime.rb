340b9ff76e447a27fe14c06414de220ba91abe24
Damely Tineo <damely.tineo07@gmail.com>
2020-01-12 01:13:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,17 +1,19 @@
 
 def prime?(num)
+  # convert num to array
   array = (1..num).to_a  
+  
+  # make sure num > 1
   if num < 2 
     return false
   end
-  array.each do |x|
-    if array.select {|x| num % x == 0} == [1, num]
-      return true
-    else 
-      return false
-    end
-  end
-end
   
-
+  # make sure num is only divisible by 1 and itself
+  if array.select {|x| num % x == 0} == [1, num]
+    return true
+  else 
+    return false
+  end
+  
+end
   
